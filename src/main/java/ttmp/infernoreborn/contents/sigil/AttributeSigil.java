d145a56bfe04658228b8c6619c1965d26cf28c02
Tictim <ksw07149@gmail.com>
2021-07-16 03:13:28 +0900

    fucking mixins, there's also sigil whatever thing i guess
    Signed-off-by: Tictim <ksw07149@gmail.com>

--- src/main/java/ttmp/infernoreborn/contents/sigil/AttributeSigil.java
+++ src/main/java/ttmp/infernoreborn/contents/sigil/AttributeSigil.java
@@ -5,45 +5,92 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.util.Hand;
 import ttmp.infernoreborn.contents.sigil.context.ItemContext;
 import ttmp.infernoreborn.contents.sigil.context.SigilEventContext;
+import ttmp.infernoreborn.util.LivingUtils;
 
 import javax.annotation.Nullable;
+import java.util.EnumMap;
+import java.util.UUID;
 
 public abstract class AttributeSigil extends Sigil{
 	public AttributeSigil(Properties properties){
 		super(properties);
 	}
 
-	@Override public void applyAttributes(SigilEventContext ctx, @Nullable EquipmentSlotType equipmentSlotType, ListMultimap<Attribute, AttributeModifier> attributes){
-		if(equipmentSlotType==null) applyBodyAttributes(ctx, attributes);
-		else{
-			ItemContext itemContext = ctx.getAsItemContext();
-			if(itemContext==null||!shouldApplyAttribute(itemContext, equipmentSlotType)) return;
-			switch(equipmentSlotType){
-				case MAINHAND:
-					applyHeldAttributes(itemContext, Hand.MAIN_HAND, attributes);
-					break;
-				case OFFHAND:
-					applyHeldAttributes(itemContext, Hand.OFF_HAND, attributes);
-					break;
-				case HEAD:
-				case LEGS:
-				case CHEST:
-				case FEET:
-					applyArmorAttributes(itemContext, equipmentSlotType, attributes);
-					break;
-			}
-		}
+	@Override public boolean canBeAttachedTo(SigilEventContext context){
+		ItemContext ictx = context.getAsItemContext();
+		return canBeAttachedTo(context, ictx!=null ? MobEntity.getEquipmentSlotForItem(ictx.stack()) : null);
 	}
 
-	protected abstract void applyBodyAttributes(SigilEventContext ctx, ListMultimap<Attribute, AttributeModifier> attributes);
-	protected abstract void applyHeldAttributes(ItemContext ctx, Hand hand, ListMultimap<Attribute, AttributeModifier> attributes);
-	protected abstract void applyArmorAttributes(ItemContext ctx, EquipmentSlotType armorType, ListMultimap<Attribute, AttributeModifier> attributes);
+	private Mode modeCache; // random states to save one parameter, because why not
 
-	protected boolean shouldApplyAttribute(ItemContext ctx, EquipmentSlotType equipmentSlotType){
-		EquipmentSlotType equipmentSlotForItem = MobEntity.getEquipmentSlotForItem(ctx.stack());
+	@Override public void applyAttributes(SigilEventContext ctx, @Nullable EquipmentSlotType equipmentSlotType, ListMultimap<Attribute, AttributeModifier> attributes){
+		if(!shouldApplyAttribute(ctx, equipmentSlotType)) return;
+		Mode mode;
+		if(equipmentSlotType==null) mode = Mode.BODY;
+		else switch(equipmentSlotType){
+			case MAINHAND:
+				mode = Mode.MAINHAND;
+				break;
+			case OFFHAND:
+				mode = Mode.OFFHAND;
+				break;
+			case FEET:
+				mode = Mode.FEET;
+				break;
+			case LEGS:
+				mode = Mode.LEGS;
+				break;
+			case CHEST:
+				mode = Mode.CHEST;
+				break;
+			case HEAD:
+				mode = Mode.HEAD;
+				break;
+			default:
+				throw new IllegalStateException("Fix the fucking bug @Tictim");
+		}
+		modeCache = mode;
+		applyAttributes(mode, attributes);
+		modeCache = null;
+	}
+
+	protected abstract boolean canBeAttachedTo(SigilEventContext ctx, @Nullable EquipmentSlotType equipmentSlotType);
+
+	protected boolean shouldApplyAttribute(SigilEventContext ctx, @Nullable EquipmentSlotType equipmentSlotType){
+		if(equipmentSlotType==null) return true;
+		ItemContext ictx = ctx.getAsItemContext();
+		if(ictx==null) return true;
+		EquipmentSlotType equipmentSlotForItem = MobEntity.getEquipmentSlotForItem(ictx.stack());
 		return equipmentSlotForItem==equipmentSlotType;
+	}
+
+	protected abstract void applyAttributes(Mode mode, ListMultimap<Attribute, AttributeModifier> attributes);
+
+	private final EnumMap<Mode, UUID> defaultUuids = new EnumMap<>(Mode.class);
+
+	protected UUID getDefaultUuidFor(Mode mode){
+		return defaultUuids.computeIfAbsent(mode, m -> UUID.randomUUID());
+	}
+
+	protected void addToModifier(ListMultimap<Attribute, AttributeModifier> attributeMap, Attribute attribute, double amount, AttributeModifier.Operation operation){
+		LivingUtils.addToModifier(attributeMap, attribute, null, getDefaultUuidFor(modeCache), amount, operation);
+	}
+	protected void addToModifier(ListMultimap<Attribute, AttributeModifier> attributeMap, Attribute attribute, @Nullable UUID targetUuid, double amount, AttributeModifier.Operation operation){
+		LivingUtils.addToModifier(attributeMap, attribute, targetUuid, targetUuid!=null ? targetUuid : getDefaultUuidFor(modeCache), amount, operation);
+	}
+	protected void addToModifier(ListMultimap<Attribute, AttributeModifier> attributeMap, Attribute attribute, @Nullable UUID targetUuid, @Nullable UUID fallbackUuid, double amount, AttributeModifier.Operation operation){
+		LivingUtils.addToModifier(attributeMap, attribute, targetUuid, fallbackUuid!=null ? fallbackUuid : getDefaultUuidFor(modeCache), amount, operation);
+	}
+
+	public enum Mode{
+		BODY,
+		MAINHAND,
+		OFFHAND,
+		HEAD,
+		LEGS,
+		CHEST,
+		FEET
 	}
 }
