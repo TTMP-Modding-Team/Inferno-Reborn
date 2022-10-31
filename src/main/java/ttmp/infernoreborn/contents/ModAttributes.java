caa7bfb70ec42c54228cf292178203f224794ae1
Tictim <ksw07149@gmail.com>
2022-10-31 13:34:24 +0900

    no korean in source code
    Signed-off-by: Tictim <ksw07149@gmail.com>

--- src/main/java/ttmp/infernoreborn/contents/ModAttributes.java
+++ src/main/java/ttmp/infernoreborn/contents/ModAttributes.java
@@ -20,22 +20,22 @@ public final class ModAttributes{
 
 	public static final DeferredRegister<Attribute> REGISTER = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, MODID);
 
-	/** 마법이 아닌 모든 종류의 간접 피해량이 N만큼 상승 */
+	/** Increases non-magic indirect damages by N */
 	public static final RegistryObject<RangedAttribute> RANGED_ATTACK = attribute("ranged_attack", 0, -1024, 1024);
-	/** 마법의 피해량이 N만큼 상승 */
+	/** Increases magic damages by N */
 	public static final RegistryObject<RangedAttribute> MAGIC_ATTACK = attribute("magic_attack", 0, -1024, 1024);
-	/** 무적시간이 아닌 매 틱 당 체력 재생 */
+	/** Generates HP by N every non-invulnerable ticks */
 	public static final RegistryObject<RangedAttribute> REGENERATION = attribute("regeneration", 0, 0, 1024);
 
-	/** 퍼센트 단위 피해 면역 */
+	/** Percentage-based damage resistance */
 	public static final RegistryObject<RangedAttribute> DAMAGE_RESISTANCE = attribute("damage_resistance", 1, 1, 2);
-	/** 퍼센트 단위 낙뎀 면역 */
+	/** Percentage-based fall damage resistance */
 	public static final RegistryObject<RangedAttribute> FALLING_DAMAGE_RESISTANCE = attribute("falling_damage_resistance", 1, 1, 2);
-	/** 퍼센트 단위 비마법 직접 피해 면역 */
+	/** Percentage-based non-magic direct damage resistance */
 	public static final RegistryObject<RangedAttribute> MELEE_DAMAGE_RESISTANCE = attribute("melee_damage_resistance", 1, 1, 2);
-	/** 퍼센트 단위 비마법 간접 피해 면역 */
+	/** Percentage-based non-magic indirect damage resistance */
 	public static final RegistryObject<RangedAttribute> RANGED_DAMAGE_RESISTANCE = attribute("ranged_damage_resistance", 1, 1, 2);
-	/** 퍼센트 단위 마법 피해 면역 */
+	/** Percentage-based magic damage resistance */
 	public static final RegistryObject<RangedAttribute> MAGIC_DAMAGE_RESISTANCE = attribute("magic_damage_resistance", 1, 1, 2);
 
 	private static RegistryObject<RangedAttribute> attribute(String name, double def, double min, double max){
